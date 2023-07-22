e51c748d9f0e6b8a8d7f2004117f320c6d5e61c6
adrian_sun <ziqingyix@gmail.com>
2023-07-22 18:25:19 +1000

    routing to default and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    loadChildren:() => import('./mymodule/login/login.module').then(m=>m.LoginModule)
+  },
+  {
+    path:'default',
+    loadChildren:() => import('./mymodule/default/default.module').then(m=>m.DefaultModule)
+  },
+  {
+    path:'**',
+    redirectTo:"/login"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/mymodule/default/default-routing.module.ts
+++ src/app/mymodule/default/default-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './default.component';
+import { HomeComponent } from './home/home.component';
+import { ReportComponent } from './report/report.component';
+import { KeywordsComponent } from './keywords/keywords.component';
+import { AlarmComponent } from './alarm/alarm.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:"",
+    component:DefaultComponent,
+    children:[
+      {
+        path:"home",
+        component:HomeComponent
+      },
+      {
+        path:"report",
+        component:ReportComponent
+      },
+      {
+        path:"keywords",
+        component:KeywordsComponent
+      },
+      {
+        path:"alarm",
+        component:AlarmComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
