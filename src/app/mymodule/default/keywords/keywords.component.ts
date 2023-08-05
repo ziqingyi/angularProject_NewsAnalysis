3eba032a698ef4f08aad8af54cbfaf3e586248a6
adrian_sun <ziqingyix@gmail.com>
2023-08-05 22:18:31 +1000

    keyword table

--- src/app/mymodule/default/keywords/keywords.component.ts
+++ src/app/mymodule/default/keywords/keywords.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class KeywordsComponent {
 
+
+  listOfData = [
+    {
+      keyword: 'cost',
+      may_include: 'profit',
+      not_include: 'sales',
+      frequency: '100'
+    },
+    {
+      keyword: 'sales',
+      may_include: 'increase',
+      not_include: 'decrease',
+      frequency: '100'
+    },
+  ];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
 }
