3e3bbc5e42787dd6bfbec4f8ab09051b46c9ed30
adrian_sun <ziqingyix@gmail.com>
2023-08-06 11:13:40 +1000

    alarm page

--- src/app/mymodule/default/alarm/alarm.component.ts
+++ src/app/mymodule/default/alarm/alarm.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./alarm.component.scss']
 })
 export class AlarmComponent {
+  alarm={
+    phone:"",
+    email:""
+  }
+
+  constructor(){
+
+  }
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  
+}
 
 }
