06d783a2fa0de549b9772d36f097b83bb0de1ca1
adrian_sun <ziqingyix@gmail.com>
2023-08-06 19:18:48 +1000

    login logic

--- src/app/mymodule/login/login.component.ts
+++ src/app/mymodule/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpService } from '../../service/http.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ export class LoginComponent {
 
   captchaData = {
     svgImg: "",
-    svgKey:""
+    // svgKey:""
   }
   loginData = {
     username: "",
@@ -23,7 +24,7 @@ export class LoginComponent {
     //Add 'implements AfterContentInit' to the class.
     this.getCaptcha();
   }
-  constructor(private http: HttpService) {
+  constructor(private http: HttpService, private router:Router) {
 
   }
   ngOnInit(): void {
@@ -33,14 +34,21 @@ export class LoginComponent {
   }
 
   getCaptcha() {
-
+    console.log('getCaptcha()');
     var api = "http://yuqing.itying.com/api/captcha";
     this.http.get(api).then((response: any) => {
+      console.log('response:');
       console.log(response);
+
       this.captchaData.svgImg = response.data.svgImg;
-      this.captchaData.svgKey = response.data.svgKey;
+      this.loginData.svgKey = response.data.svgKey;
       
+      console.log('captchaData and loginData');
+      console.log(this.captchaData);
+      console.log(this.loginData);
+
       const svgImgDom:HTMLElement|null = document.querySelector("#svgImg");
+      console.log('svgImgDom:');
       console.log(svgImgDom);
       if(svgImgDom){
         svgImgDom.innerHTML = this.captchaData.svgImg;
@@ -48,8 +56,29 @@ export class LoginComponent {
     }
     );
   }
+  //admin 123456
   doLogin() {
+    console.log('doLogin()');
 
+    console.log('captchaData and loginData');
+    console.log(this.captchaData);
+    console.log(this.loginData);
+
+    var api="http://yuqing.itying.com/api/doLogin";
+
+    this.http.post(api,this.loginData)
+             .then((response:any)=>{
+                      console.log(response);
+
+                      if(response.data.success)
+                      {
+                        //router to default module
+                        this.router.navigate(['/default']);// inject angular router service
+                      }
+                      else{
+                        alert(response.data.message)
+                      }
+                   });
   }
 
 
