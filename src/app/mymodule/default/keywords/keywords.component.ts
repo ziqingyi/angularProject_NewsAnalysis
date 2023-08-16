e8c865cb1b038a19f2fc4048529ec85d9c11527d
adrian_sun <ziqingyix@gmail.com>
2023-08-16 21:43:18 +1000

    display keywords from api

--- src/app/mymodule/default/keywords/keywords.component.ts
+++ src/app/mymodule/default/keywords/keywords.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpService } from '../../../service/http.service';
+import { StorageService } from '../../../service/storage.service';
 
 @Component({
   selector: 'app-keywords',
@@ -10,6 +12,10 @@ export class KeywordsComponent {
   isVisible = false;
   isOkLoading= false;//Whether to apply loading visual effect for OK button or not
 
+
+  userinfo:any;
+
+
   inputData={
     keyword:"",
     may_keyword:"",
@@ -20,26 +26,27 @@ export class KeywordsComponent {
   listOfData = [
     {
       keyword: 'cost',
-      may_include: 'profit',
-      not_include: 'sales',
+      may_keyword: 'profit',
+      nokeyword: 'sales',
       frequency: '100'
     },
     {
       keyword: 'sales',
-      may_include: 'increase',
-      not_include: 'decrease',
+      may_keyword: 'increase',
+      nokeyword: 'decrease',
       frequency: '100'
     },
   ];
 
-  constructor(){
-
+  constructor(private http:HttpService,private storage:StorageService){
+    this.userinfo = this.storage.get("userinfo");
+    console.log(this.userinfo);
   }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    
+    this.getkeywords();
   }
 
    showModal(): void {
@@ -58,6 +65,25 @@ export class KeywordsComponent {
     this.isVisible = false;
   }
 
+  getkeywords(){
+     var api = "/api/keywordsList";
+    this.http.getWithConfig(api,{
+      auth:{
+        username:this.userinfo.token,
+        password:""
+      }
+    }).then((response:any)=>{
+      console.log("response from keywordsList: ");
+       console.log(response);
+      if(response.data.success == true){
+        this.listOfData=response.data.result;
+      }
+      
+    })
+
+
+
+  }
 
 
 }
