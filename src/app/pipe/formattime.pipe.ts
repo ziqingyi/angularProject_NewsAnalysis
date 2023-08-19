6b5b7f46063545004163f5fd12284e2f8282bc6b
adrian_sun <ziqingyix@gmail.com>
2023-08-19 21:20:39 +1000

    use silly datetime

--- src/app/pipe/formattime.pipe.ts
+++ src/app/pipe/formattime.pipe.ts
@@ -1,5 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+// import sd from 'silly-datetime';
+// import {format,fromNow} from 'silly-datetime';
+import sd from 'silly-datetime'
+// var sd = require('silly-datetime');
+
 @Pipe({
   name: 'formattime'
 })
@@ -7,9 +12,11 @@ export class FormattimePipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
 
-    var d = new Date(value);
-    d.getFullYear;
-    console.log("format time pipe: " + d);
+    // var d = new Date(value);
+    // d.getFullYear;
+    // console.log("format time pipe: " + d);
+    //value is 10 digits timestamp, js will need to use 13 digits timestamp
+    value = sd.format(value *1000,'YYYY-MM-DD HH:mm');
     return value;
   }
 
